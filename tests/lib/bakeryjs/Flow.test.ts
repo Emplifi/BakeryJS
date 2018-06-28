9c8353062c2fddb7d7f99f8e31e241a230a9cf57
Martin Štekl <martin.stekl@gmail.com>
2018-06-28 11:51:02 +0200

    add simple test for Flow as an example for other tests

---
+++ tests/lib/bakeryjs/Flow.test.ts
@@ -0,0 +1,16 @@
+import {priorityQueue} from 'async';
+import {Flow} from '../../../src/lib/bakeryjs/Flow';
+import {Job} from '../../../src/lib/bakeryjs/Job';
+import {Message} from '../../../src/lib/bakeryjs/Message';
+
+describe('Flow', () => {
+    it('enqueues job as a message', () => {
+        const queue = priorityQueue((task: Message) => null, 1);
+        const spyOnPush = jest.spyOn(queue, 'push');
+
+        const flow = new Flow(queue);
+        flow.process(new Job());
+
+        expect(spyOnPush).toHaveBeenCalledTimes(1);
+    });
+});
