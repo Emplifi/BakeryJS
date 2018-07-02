992e4cea0b6b69ed361fd0e5045afbc0f5759c03
Martin Štekl <martin.stekl@gmail.com>
2018-07-02 14:23:13 +0200

    add tests for message input/output handling

--- src/lib/bakeryjs/Message.ts
+++ src/lib/bakeryjs/Message.ts
@@ -22,6 +22,12 @@ export class Message {
     }
 
     setOutput(provides: string[], output: MessageData): void {
+        const currentKeys = Object.keys(this.data);
+        const intersectionKeys = currentKeys.filter((key: string) => provides.indexOf(key) !== -1);
+        if (intersectionKeys.length > 0) {
+            throw new Error(`Cannot provide some data because the message already contains following results "${intersectionKeys.join('", "')}".`);
+        }
+
         debug(`set output: ${JSON.stringify(output)}`);
         for (const p of provides) {
             this.data[p] = output[p];
---
+++ tests/lib/bakeryjs/Message.test.ts
@@ -0,0 +1,72 @@
+import {Message} from '../../../src/lib/bakeryjs/Message';
+
+describe('Message', () => {
+    const inputTestData = [
+        {
+            init: {},
+            requires: [],
+            expected: {},
+        },
+        {
+            init: {foo: 0, bar: 1},
+            requires: ['foo'],
+            expected: {foo: 0},
+        },
+        {
+            init: {foo: 0, bar: 1},
+            requires: ['baz'],
+            expected: {baz: undefined},
+        },
+    ];
+
+    inputTestData.forEach(({init, requires, expected}, index: number) => {
+        it(`returns partial data as input #${index}`, () => {
+            const message = new Message(init);
+            const input = message.getInput(requires);
+            expect(input).toEqual(expected);
+        });
+    });
+
+    const outputTestData = [
+        {
+            init: {},
+            provides: [],
+            data: {},
+            expected: {},
+        },
+        {
+            init: {},
+            provides: ['foo'],
+            data: {foo: 0},
+            expected: {foo: 0},
+        },
+        {
+            init: {bar: 1},
+            provides: ['foo'],
+            data: {foo: 0},
+            expected: {bar: 1, foo: 0},
+        },
+        {
+            init: {},
+            provides: ['foo'],
+            data: {},
+            expected: {foo: undefined},
+        },
+    ];
+
+    outputTestData.forEach(({init, provides, data, expected}, index: number) => {
+        it(`saves output data to the message #${index}`, () => {
+            const message = new Message(init);
+            message.setOutput(provides, data);
+            expect(message.data).toEqual(expected);
+        });
+    });
+
+    it('throws an error on rewriting existing data key by an output without any changes', () => {
+        const message = new Message({foo: 0, bar: 1});
+        expect(() => {
+            message.setOutput(['baz', 'bar'], {baz: 2, bar: 3});
+        }).toThrowError(new Error('Cannot provide some data because the message already contains following results "bar".'));
+        expect(message.data).toEqual({foo: 0, bar: 1});
+    });
+});
