77376dda0481602fb3162797e3f806a7082485f1
Martin Štekl <martin.stekl@gmail.com>
2018-07-16 15:17:14 +0200

    fix formatting in tests for pretier

--- tests/lib/bakeryjs/Flow.test.ts
+++ tests/lib/bakeryjs/Flow.test.ts
@@ -4,13 +4,13 @@ import {MemoryPriorityQueue} from '../../../src/lib/bakeryjs/queue/MemoryPriorit
 import {Message} from '../../../src/lib/bakeryjs/Message';
 
 describe('Flow', () => {
-    it('enqueues job as a message', () => {
-        const queue = new MemoryPriorityQueue((task: Message) => undefined, 1);
-        const spyOnPush = jest.spyOn(queue, 'push');
+	it('enqueues job as a message', () => {
+		const queue = new MemoryPriorityQueue((task: Message) => undefined, 1);
+		const spyOnPush = jest.spyOn(queue, 'push');
 
-        const flow = new Flow(queue);
-        flow.process(new Job());
+		const flow = new Flow(queue);
+		flow.process(new Job());
 
-        expect(spyOnPush).toHaveBeenCalledTimes(1);
-    });
+		expect(spyOnPush).toHaveBeenCalledTimes(1);
+	});
 });
--- tests/lib/bakeryjs/componentNameParser.test.ts
+++ tests/lib/bakeryjs/componentNameParser.test.ts
@@ -1,39 +1,39 @@
 import {parseComponentName} from '../../../src/lib/bakeryjs/componentNameParser';
 
 type PathTestDefinition = {
-    path: string,
-    expected: string | null,
+	path: string;
+	expected: string | null;
 };
 
 describe('component name parser', () => {
-    const paths: PathTestDefinition[] = [
-        {
-            path: '_/boxes/generators/processors/test-001.coffee.js.ts',
-            expected: 'test-001',
-        },
-        {
-            path: 'test-002.ts',
-            expected: 'test-002',
-        },
-        {
-            path: '_/.secret/test-003.ts',
-            expected: '.secret/test-003',
-        },
-        {
-            path: '.gitignore',
-            expected: null,
-        },
-        {
-            path: '_/',
-            expected: null,
-        },
-    ];
+	const paths: PathTestDefinition[] = [
+		{
+			path: '_/boxes/generators/processors/test-001.coffee.js.ts',
+			expected: 'test-001',
+		},
+		{
+			path: 'test-002.ts',
+			expected: 'test-002',
+		},
+		{
+			path: '_/.secret/test-003.ts',
+			expected: '.secret/test-003',
+		},
+		{
+			path: '.gitignore',
+			expected: null,
+		},
+		{
+			path: '_/',
+			expected: null,
+		},
+	];
 
-    paths.forEach(({path, expected}: PathTestDefinition, index: number) => {
-        it(`parses name from component file path #${index}`, () => {
-            const name = parseComponentName(path);
+	paths.forEach(({path, expected}: PathTestDefinition, index: number) => {
+		it(`parses name from component file path #${index}`, () => {
+			const name = parseComponentName(path);
 
-            expect(name).toBe(expected);
-        });
-    });
+			expect(name).toBe(expected);
+		});
+	});
 });
