b4bf348d408821dfb8411a121474523b4cec1b04
Timothy Breeding <tbreeding@purestorage.com>
2025-12-14 10:33:37 +0100

    Add coverage script with enhanced Jest coverage configuration

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,17 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	collectCoverageFrom: ['src/**/*.{js,ts}'],
-	coverageReporters: ['text', 'lcov'],
+	collectCoverageFrom: ['src/**/*.{js,ts}', '!src/**/*.d.ts'],
+	coverageDirectory: 'coverage',
+	coverageReporters: ['text', 'text-summary', 'lcov', 'html', 'json'],
+	coverageThreshold: {
+		global: {
+			branches: 50,
+			functions: 50,
+			lines: 50,
+			statements: 50,
+		},
+	},
 	moduleNameMapper: {
 		'^bakeryjs$': '<rootDir>/src/index.ts',
 		'^bakeryjs/(.*)$': '<rootDir>/src/lib/bakeryjs/$1',
